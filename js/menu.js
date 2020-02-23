6147b4951ce544415aa113a491c719c4b343e70c
vipma <vip.maksmax@gmail.com>
2020-02-23 23:30:23 +0400

    add react

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const element = (
+    <div>
+        <table>
+            <tr>
+                <td><a className="btnfos btnfos-1" href="#" align="center">Главная</a></td>
+                <td><a className="btnfos btnfos-1" href="about.html" align="center">Обо мне</a></td>
+                <td><a className="btnfos btnfos-1" href="portfolio.html" align="center">Портфолио</a></td>
+                <td><a className="btnfos btnfos-1" href="resume.html" align="center">Резюме</a></td>
+                <td><a className="btnfos btnfos-1" href="contact.html" align="center">Контакты</a></td>
+            </tr>
+        </table>
+    </div>
+);
+ReactDOM.render(element, document.getElementById('menu'));
